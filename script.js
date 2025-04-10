b2789658e36cecd850149251e461d2f5767ec5da
evgeniihram <EvgeniiHram@proton.me>
2025-04-10 09:06:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,218 @@
+const pages = {
+  pray: `
+
+   <p>
+
+  <img src="evgenii.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+      <p>
+<h2>Молитвенная помощь и поддержка</h2><p>      
+      <p>
+По заявкам конфиденциально: 
+<br><br>
+Оказываю молитвенную помощь и поддержку. В том числе и о том, за что другие отказывают. <br>
+<br>
+
+
+
+Молю Бога о здравии, богатстве и деньгах, счастье и удаче, успехе и благополучии, избавлении от бед и о процветании.
+<br><br>
+Осуществляю молитвенное сопровождение сделок, проектов, стартапов и других мероприятий.
+<br><br>
+Молитвенное сопровождение бизнеса.
+<br><br>
+Молитвенная профилактика. В том числе проблем с налоговыми и правоохранительными органами. <br>
+<br>
+Осуществляю экстренную неотложную молитвенную помощь и поддержку по заявкам в конкретное время и по конкретному случаю.
+<br><br>
+Поминаю о здравии и о упокоении.
+<br><br>
+Личное молитвенное сопровождение. 
+<br><br>
+Пишите ваши просьбы. Пожертвования приветствуются. 
+
+<br><br>
+    <br>Email:
+ <a href="mailto:EvgeniiHram@outlook.com" target="_blank">EvgeniiHram@outlook.com</a>
+ <br>Telegram: 
+ <a href="https://t.me/EvgeniiHram" target="_blank">@EvgeniiHram</a>
+   <br>WhatsApp:
+ <a href="https://wa.me/message/GEDYZ3IQQLGBM1" target="_blank">Келейный храм Евгения</a>
+ <br>X: 
+ <a href="https://x.com/EvgeniiHram" target="_blank">@EvgeniiHram</a>
+   <br>Instagram:
+ <a href="https://www.instagram.com/evgeniihram" target="_blank">@EvgeniiHram</a>
+ <br>Bastyon: 
+ <a href="https://bastyon.com/evgeniihram" target="_blank">@EvgeniiHram</a>
+
+
+
+
+</p>
+`,
+
+  akaf: `   
+
+<h2>Акафисты, каноны, молитвы</h2><p>
+<br>
+    
+ <a href="https://azbyka.ru/molitvoslov/kanon-svjatym-muchenikam-evgeniju-kanidiju-ualerianu-i-akile-trapezundskim.html" target="_blank">Канон святым мученикам Евгению, Канидию, Уалериану и Акиле Трапезундским</a>
+ <br><br>
+ <a href="https://azbyka.ru/molitvoslov/kanon-svjatym-muchenikam-v-melitine-postradavshim.html" target="_blank">Канон святым мученикам, в Мелитине пострадавшим</a>
+   <br><br>
+ <a href="https://azbyka.ru/molitvoslov/kanon-svjatym-svjashhennomuchenikam-v-hersone-episkopstvovavshim-vasiliju-efremu-kapitonu-evgeniju-eferiju-i-prochim.html" target="_blank">Канон святым священномученикам, в Херсоне епископствовавшим: Василию, Ефрему, Капитону, Евгению, Еферию и прочим</a>
+ <br><br>
+ <a href="https://azbyka.ru/molitvoslov/kanon-muchenikam-evstratiju-avksentiju-evgeniju-mardariju-i-orestu-sevastijskim.html" target="_blank">Канон мученикам Евстратию, Авксентию, Евгению, Мардарию и Оресту Севастийским</a>
+   <br><br>
+ <a href="https://azbyka.ru/molitvoslov/akafist-svjatomu-novomucheniku-vrachu-evgeniju-botkinu.html" target="_blank">Акафист святому новомученику врачу Евгению Боткину</a>
+ <br><br>
+ <a href="https://azbyka.ru/otechnik/Dimitrij_Muretov/akafist-svjatym-svjashennomuchenikam-vasiliyu-efremu-evgeniyu-elpidiyu-agafodoru-eferiyu-i-kapitonu-v-hersone-episkopstvovavshim/" target="_blank">Акафист священномученикам Василию, Ефрему, Евгению, Елпидию, Агафодору, Еферию и Капитону, в Херсоне епископствовавшим</a>
+ <br><br>
+ <a href="https://azbyka.ru/molitvoslov/akafist-svjatoj-prepodobnomuchenice-evgenii-rimskoj.html" target="_blank">Акафист святой преподобномученице Евгении Римской</a>
+</p>`,
+
+
+heaven: `
+   
+<h2>Небесные покровители</h2><p>
+<br>
+    
+ <a href="https://azbyka.ru/days/sv-evgenij-melitinskij" target="_blank">Мученик Евге́ний Мелитинский  <br>20 ноября</a>
+ <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenij-sevastijskij" target="_blank">Мученик Евге́ний Севастийский 
+<br>26 декабря</a>
+   <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenija-rimskaja" target="_blank">Преподобномученица Евгения Римская, дева<br>
+6 января</a>
+ <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenij-trapezundskij" target="_blank">Мученик Евге́ний Трапезундский<br>
+3 февраля</a>
+   <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenij-vifinskij" target="_blank">Преподобный Евге́ний Вифинский<br>
+25 февраля</a>
+ <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenij-hersonesskij" target="_blank">Священномученик Евге́ний Херсонесский, епископ<br>
+20 марта, 28 декабря</a>
+ <br><br>
+ <a href="https://azbyka.ru/days/sv-evgenij-botkin-vrach-strastoterpec" target="_blank">Праведный Евге́ний Боткин, врач, страстотерпец<br>
+17 июля</a>
+</p>`,
+
+
+  minjust: `<h2>Подтверждения Министерства юстиции РФ</h2>
+
+<p>
+Управление Министерства юстиции Российской Федерации по Иркутской области.<br>
+16 августа 2024 № 38/03-7566. <br>
+Ведомственный № религиозной группы 3818010190.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Республике Татарстан. <br>
+22.08.2024 № 16/03-4788.<br>
+Ведомственный № религиозной группы 1618010085.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Ханты-Мансийскому автономному округу. <br>
+27.08.2024 № 86/03-3159.<br>
+Ведомственный № религиозной группы 8618010071.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Ярославской области. <br>
+28.08.2024 № 76/03-3966.<br>
+Ведомственный № религиозной группы 7618010032.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Курганской области. <br>
+29.08.2024 № 45/03-2451.<br>
+Ведомственный № религиозной группы 4518010089.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Томской области. <br>
+30.08.2024 № 70/03-2749.<br>
+Ведомственный № религиозной группы 7018010047.
+<br><br>
+
+Управление Министерства юстиции Российской Федерации по Астраханской области. <br>
+02.09.2024 № 30/03-3250. <br>
+Ведомственный № религиозной группы 3018010036.<br><br>
+
+Управление Министерства юстиции Российской Федерации по Пермскому краю. <br>
+02.09.2024 № 59/03-3875.<br>
+Ведомственный № религиозной группы 5918010093.<br><br>
+
+Управление Министерства юстиции Российской Федерации по Воронежской области. <br>
+03.09.2024 № 36/03-5503.<br>
+Ведомственный № религиозной группы 3618010091.<br><br>
+
+Управление Министерства юстиции Российской Федерации по Алтайскому краю. <br>
+09.09.2024 № 22/03-4783. <br>
+Ведомственный № религиозной группы 2218010084.<br><br>
+
+Управление Министерства юстиции Российской Федерации по Оренбургской области. <br>
+11.09.2024 № 56/03-3530. <br>
+Ведомственный № религиозной группы 5618010111.<br><br>
+</p>
+
+<img src="altai.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+
+<img src="irkutsk.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+
+<img src="tomsk.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+
+<img src="jarosl.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+
+<img src="hmao.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+
+`,
+
+
+
+  contacts: `
+   <p>
+
+  <img src="evgenii.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+      <p>
+<h2>Контакты</h2><p>По всем вопросам:  <br>Email:
+ <a href="mailto:EvgeniiHram@outlook.com" target="_blank">EvgeniiHram@outlook.com</a>
+ <br>Telegram: 
+ <a href="https://t.me/EvgeniiHram" target="_blank">@EvgeniiHram</a>
+   <br>WhatsApp:
+ <a href="https://wa.me/message/GEDYZ3IQQLGBM1" target="_blank">Келейный храм Евгения</a>
+ <br>X: 
+ <a href="https://x.com/EvgeniiHram" target="_blank">@EvgeniiHram</a>
+   <br>Instagram:
+ <a href="https://www.instagram.com/evgeniihram" target="_blank">@EvgeniiHram</a>
+ <br>Bastyon: 
+ <a href="https://bastyon.com/evgeniihram" target="_blank">@EvgeniiHram</a>
+</p>`,
+
+  donate: `   <p>
+
+  <img src="evgenii.jpg" alt="Келейный храм Евгения" style="max-width:100%;margin-top:20px;">
+      <p>
+
+<h2>Пожертвовать</h2><p></p><ul>
+        <li>Карта ВТБ: 2200 2407 5492 3262</li>    
+
+<br>
+<li>Через систему быстрых платежей (СБП) по номеру телефона: <br>+7 (992) 090 44 48 (Альфа-Банк)</li><br>
+    <li>Криптовалюта: <br>
+BTC<br>
+bc1qhckevdwqsshxs584d4709uxy0c7w4w08naf9xa
+<br>
+USDT
+<br>
+0x688cdFa8463aFC1D517b556d51342f081d993947
+<br>
+XRP
+<br>
+rDtEucEBf5KoFGNmt93138Jt3cSe6Gv7xV
+<br>
+</li>
+  
+    
+  </ul>`
+};
+
+function loadPage(page) {
+  document.getElementById('content').innerHTML = pages[page] || '<p>Страница не найдена.</p>';
+}
